4b34bee223181110c40dbd40aed09235a6ea9d8e
Bianca Chami <b.chami39@gmail.com>
2023-12-18 09:52:51 +0000

    feat: fixed document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,6 @@ function Document() {
             src="https://static.zdassets.com/ekr/snippet.js?key=3ca3ab96-67f1-4a75-a4ab-8e206a0a4660"
             async
           >
-            {' '}
           </script>
         }
       </Head>
